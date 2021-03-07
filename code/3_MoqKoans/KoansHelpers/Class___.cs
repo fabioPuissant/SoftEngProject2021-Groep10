170809f1ed1e7c2ae4695118cdb10b4935cf1f77
FabioP-2055346 <fabio.puissant@student.uhasselt.be>
2021-03-07 22:05:35 +0100

    added example code for Unit testing in C3

---
+++ code/3_MoqKoans/KoansHelpers/Class___.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Dynamic;
+
+namespace MoqKoansCore.KoansHelpers
+{
+	public class ___ : Exception
+	{
+		public void ____()
+		{
+		}
+
+		public dynamic Object => new ExpandoObject();
+	}
+}
---
+++ code/3_MoqKoans/KoansHelpers/Koan.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace MoqKoansCore.KoansHelpers
+{
+	public class Koan
+	{
+		private const string EMPTY_ASSERT_MSG = "Replace the method call \"Assert___()\" with an appropriate Assert statement.";
+		private const string EMPTY_ANSWER_MSG = "Replace \"___\" with your answer to make the test pass.";
+
+		private static readonly object _obj = new object();
+
+		protected static object ___ => _obj;
+
+	    protected static int ____ => 0;
+
+	    protected void Assert___(params object[] objs)
+		{
+            
+			throw new AssertionException(EMPTY_ASSERT_MSG);
+		}
+	}
+}
